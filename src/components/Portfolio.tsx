c1be28bda3f2aa6948bac501d70cc6fdfd0cfc26
HemanthSai-99 <hemanthsaitholikonda99@gmail.com>
2025-09-03 10:15:23 +0530

    Updated Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -6,9 +6,9 @@ const Portfolio = () => {
     {
       title: 'Adactin Hotel App',
       description: 'Performed end-to-end testing on Adactin Hotel App including login, hotel search, booking, and payment workflows. Covered functional, UI, and regression testing scenarios.',
-      image: 'https://adactinhotelapp.com/',
+      image: 'https://adactinhotelapp.com/img/main-image-hotel-app.jpeg',
       technologies: ['Manual Testing', 'Defect Tracking'],
-      liveUrl: '#',
+      liveUrl: 'https://adactinhotelapp.com/',
       githubUrl: '#'
     },
     // {
