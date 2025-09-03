2a04572a20f83b5e6ff27af175ad6b84a6d2a82a
HemanthSai-99 <hemanthsaitholikonda99@gmail.com>
2025-09-03 09:38:40 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
           <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-white/20 shadow-2xl">
             <img
               src="https://lh3.googleusercontent.com/a/ACg8ocLlhVaE0xUzA8p1tUUbflRAtqBRjPG3lznq8cjWuAwdazUhg9s=s288-c-no"
-              alt="Alex Johnson"
+              alt="Hemanth Sai Tholikonda"
               className="w-full h-full object-cover"
             />
           </div>
